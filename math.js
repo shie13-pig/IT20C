c98793d08021768bfd5d40ccea36d7f94f8d689d
shie13-pig <shiela07132020@gmail.com>
2025-11-17 08:01:32 -1100

    Added Subtraction

---
+++ math.js
@@ -0,0 +1,20 @@
+num1 = 2;
+num2 = 3;
+
+sum = num1 + num2;
+ 
+ console.log(sum)
+
+ function sum (num1,num2){
+ sum = num1 + num2;
+ console.log(sum);
+ }
+
+ sum(2,3);
+
+ function diff (num1,num2){
+ diff = num1 + num2;
+ console.log(diff);
+ }
+
+ diff(2,3);
