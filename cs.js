b5fece16852e9cb527997be2b0ef69844b47111b
shie13-pig <shiela07132020@gmail.com>
2025-11-17 14:39:01 -1100

    Added Log-in

--- cs.js
+++ cs.js
@@ -23,3 +23,16 @@ function computeGrade(grade){
         }
 }
 computeGrade(85);
+
+function login (username,password){
+    if(username === "shiela"){
+    if(password === "12345"){
+        console.log ("Welcome User");
+    } else {
+        console.log ("Invalid Login Credentials")
+    }
+} else {
+    console.log ("Invalid User");
+}
+}
+login ("shiela",123);
