1c32674dff49147e530251693d8a6ff9d599f201
shie13-pig <shiela07132020@gmail.com>
2025-11-20 08:27:16 -1100

    Added For-loop

---
+++ loopings.js
@@ -0,0 +1,17 @@
+//for loop
+
+for (i = 1; 1<=5; i++){
+    //console.log(i);
+    output += i + " ";
+}
+
+//console.log (output);
+
+//arrays
+arr [1,2,3];
+
+for (index =0; index <arr.length; index++){
+    //console.log(arr[i]);
+    output +=arr[index] + " ";
+}
+console.log(output);
