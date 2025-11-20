4ff7d9041c745ee3ab56c600b0286a58f7a1c3bb
shie13-pig <shiela07132020@gmail.com>
2025-11-20 08:40:07 -1100

    Added while-loop

---
+++ whileloop.js
@@ -0,0 +1,23 @@
+/*i = 5;
+
+while (i<=5){
+    console.log(i);
+    i--;
+}
+
+console.log("Ascending");
+x=1;
+while(x <= 5){
+    console.log(x);
+    x++;
+}*/
+
+arr = [1,2,3];
+output = " ";
+index = 1;
+
+while(index <= arr.lenth-1){
+    output += arr[index] + " ";
+    index++;
+}
+console.log(output);
