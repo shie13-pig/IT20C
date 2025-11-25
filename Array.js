1d35942711fe82861405cf9187330c91def31e3e
shie13-pig <shiela07132020@gmail.com>
2025-11-24 18:33:17 -1100

    Added update method

--- Array.js
+++ Array.js
@@ -36,7 +36,13 @@ search (element)
             console.log(element + "is found at index" + i);
             return-1;
         }
-        
+        upadate(index,element){
+            if (index < 0 || index >= this.length){
+                console.log("Index Index");
+                return-1;
+            }
+            this.data[index]=element;
+        }        
 arr1 = new Array ([4,2,3]);
 arr1.traverse();
 arr1.insert(3,50);
