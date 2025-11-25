7375396253fcacb1193d62de4fa4b27b852f724c
shie13-pig <shiela07132020@gmail.com>
2025-11-24 18:36:26 -1100

    Added delete method

--- Array.js
+++ Array.js
@@ -42,7 +42,19 @@ search (element)
                 return-1;
             }
             this.data[index]=element;
-        }        
+        }  
+        delete(index){
+            if (index < 0 || index >= this.length){
+                console.log("Index Index");
+                return;
+        }
+        for (let i=index;i<this.length-1;i++){
+            this.data[i]=this.data[i+1];
+        }
+        delete this.data[this.length-1];
+        this.length--;
+    }
+
 arr1 = new Array ([4,2,3]);
 arr1.traverse();
 arr1.insert(3,50);
