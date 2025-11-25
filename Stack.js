a5ebe7fe681b00de9bca6f902a2abaae81a72024
shie13-pig <shiela07132020@gmail.com>
2025-11-24 18:42:47 -1100

    Added stack and push method

---
+++ Stack.js
@@ -0,0 +1,21 @@
+class Stack{
+    constructor(){
+        this.data={};
+        this.topIndex=-1;
+}
+    push(element){
+        this.topIndex++;
+        this.data[this.topIndex]=element;
+    }
+    is_empty(){
+        return this.topIndex===-1;
+    }
+}
+
+let stack1=new Stack();
+stack1.push(2);
+stack1.traverse();
+stack1.push(3);
+stack1.traverse();
+stack1.push(1);
+stack1.traverse();
