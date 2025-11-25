a6be1a8d9095d2e35bf62551d65e6750b5b7fa40
shie13-pig <shiela07132020@gmail.com>
2025-11-24 18:56:05 -1100

    Added pop and size method

--- Stack.js
+++ Stack.js
@@ -14,6 +14,17 @@ class Stack{
         if (this.is_empty())return null;
         return this.data[this.topIndex];
     }
+    pop (){
+        if (this.is_empty()) return null;
+
+        let value = this.data[this.topIndex];
+        delete this.data[this.topIndex];
+        this.topIndex--;
+        return value;
+    }
+    size (){
+        return this.topIndex + 1;
+    }
 }
 
 let stack1=new Stack();
@@ -22,4 +33,10 @@ stack1.traverse();
 stack1.push(3);
 stack1.traverse();
 stack1.push(1);
+stack1.traverse();
+//console.log("Top element is: " + stack1.peek());
+console.log("Top element of the stack is: " + stack1.is_empty());
+stack1.pop();
+console.log("Top element " + stack1.pop()+ " is popped the new element" +stack1.peek());
+console.log("New size of the stack is: " + stack1.size());
 stack1.traverse();
