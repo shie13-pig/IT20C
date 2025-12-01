7262c3f35b41736155e268dc8b60f6cba02cbc0f
shie13-pig <shiela07132020@gmail.com>
2025-12-01 08:18:10 -1100

    Added dequeue method to lab/DSA Queueu

--- Queue.js
+++ Queue.js
@@ -16,7 +16,7 @@ class Queue{
             output += this.data[i] + " ";
         }
 
-        console.log(output);
+        return output;
     }
     is_empty(){
         return this.frontIndex > this.rearIndex;
@@ -30,14 +30,23 @@ class Queue{
     size(){
         return this.rearIndex - this.frontIndex + 1;
     }
+    dequeue(){
+        //Correction Statement
+        if(this.is_empty()) return null;
+
+        const value = this.data[this.frontIndex];
+        delete this.data[this.frontIndex];
+        this.frontIndex++;
+        return value;
+    }
 }
 
 //
 let queue1 = new Queue();
 queue1.enqueue(4);
-queue1.enqueue(3);
 queue1.enqueue(2);
+queue1.enqueue(3);
 //queue1.traverse();
-console.log(queue1.is_empty());
-console.log(queue1.front());
-console.log(queue1.size());
+console.log(queue1.traverse());
+console.log("The front value "+queue1.dequeue()+" is dequeued");
+console.log(queue1.traverse());
