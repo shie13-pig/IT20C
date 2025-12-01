6b5602cccdd08230c70fa026e98d15a1d053beda
shie13-pig <shiela07132020@gmail.com>
2025-12-01 08:03:34 -1100

    Added Initial Enqueue for Queue

--- Queue.js
+++ Queue.js
@@ -2,21 +2,33 @@ class Queue{
     constructor(){
         this.data={};
         this.frontIndex = 0;
-        this.readIndex = -1;
+        this.rearIndex = -1;
     }
 
     enqueue(element){
-        this.readIndex++
-        this.data[this.readIndex] = element;   
+        this.rearIndex++
+        this.data[this.rearIndex] = element;   
     }
 
     traverse(){
         let output = " ";
-        for (let i=this.frontIndex;i <= this.readIndex;i++){
+        for (let i=this.frontIndex;i <= this.rearIndex;i++){
             output += this.data[i] + " ";
         }
 
         console.log(output);
+    }
+    is_empty(){
+        return this.frontIndex > this.rearIndex;
+    }
+    front(){
+        //Correction Statement
+        if (this.is_empty()) return null;
+        return this.data[this.frontIndex];
+    }
+
+    size(){
+        return this.rearIndex - this.frontIndex + 1;
     }
 }
 
@@ -25,4 +37,7 @@ let queue1 = new Queue();
 queue1.enqueue(4);
 queue1.enqueue(3);
 queue1.enqueue(2);
-queue1.traverse();
+//queue1.traverse();
+console.log(queue1.is_empty());
+console.log(queue1.front());
+console.log(queue1.size());
